4ce3d98c213c3669b518bf60b51c8dbba69e301a
John W. Long <john@uservoice.com>
2013-10-13 13:24:28 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SassRegistry::Application.config.secret_key_base = 'dac085653abee817989ea2675de297a91d70039a63552da0d1ac428e2d3326f1e447ede76c0c0ed8b7227881ae40329355085890125b8a0efb08f186db59a2e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SassRegistry::Application.config.session_store :cookie_store, key: '_sass-registry_session'
