a4b90f896f911f8e7356c57db968eeb06d197143
Mina Markham <mina.markham@gmail.com>
2013-11-02 00:00:13 -0500

    add typekit script; simplify site navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,4 +49,9 @@ module ApplicationHelper
     @_markdown.render(source)
   end
 
+  def typekit_include_tag apikey
+    javascript_include_tag("//use.typekit.com/#{apikey}.js") +
+    javascript_tag("try{Typekit.load()}catch(e){}")
+  end
+  
 end
